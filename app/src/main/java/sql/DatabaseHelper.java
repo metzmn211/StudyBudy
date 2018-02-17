b743f592df793cf295642b3ef274c151750c26f9
lfg3 <louis.f.griffis@my.sfcollege.edu>
2018-02-16 21:34:29 -0500

    what i have done so far LG

---
+++ app/src/main/java/sql/DatabaseHelper.java
@@ -0,0 +1,21 @@
+package sql;
+
+/**
+ * Created by louis on 2/16/2018.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static
+}
