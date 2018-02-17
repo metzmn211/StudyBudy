83d9cdc81059ba3ae1138cad0beef4e664876735
lfg3 <louis.f.griffis@my.sfcollege.edu>
2018-02-16 23:33:17 -0500

    what i have done so far LG

--- app/src/main/java/sql/DatabaseHelper.java
+++ app/src/main/java/sql/DatabaseHelper.java
@@ -17,5 +17,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
 
-    private static
+    private static final String DATABASE_NAME = "UserManager.db";
+
+    private static final String TABLE_USER = "user";
+
+    private static final String COLUMN_USER_ID = "user_id";
+    private static final String COLUMN_USER_NAME = "user_name";
+    private static final String COLUMN_USER_EMAIL = "user_email";
+    private static final String COLUMN_USER_PASSWORD = "user_password";
+
+    private String CREATE_USER_TABLE = "CREATE TABLE" + TABLE_USER + "("
+            + COLUMN_USER_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + "TEXT,"
+            + COLUMN_USER_EMAIL + "TEXT," + COLUMN_USER_PASSWORD + "TEXT" + ")";
+
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS" + TABLE_USER;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, )
+    }
 }
